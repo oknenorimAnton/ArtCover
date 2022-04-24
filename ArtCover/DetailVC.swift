dd74e2023b9985dbdd05fb17251964859b625f37
f <f>
2022-04-24 22:27:13 +0300

    init

---
+++ ArtCover/DetailVC.swift
@@ -0,0 +1,26 @@
+//
+//  DetailVC.swift
+//  ArtCover
+//
+//  Created by Антон on 07.04.2022.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var trackTitle = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        image.image = UIImage(named: trackTitle)
+        titleLabel.text = trackTitle
+        titleLabel.numberOfLines = 0
+    }
+    
+
+}
